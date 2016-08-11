15d959d33319ab3d420b44c1d8c19eefb800588b
wen-chung <wcliao1962@gmail.com>
2016-08-11 13:44:00 +0800

    use Carbon

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 
+
+require  __DIR__.'/vendor/autoload.php';
+
 define('DB_HOST', 'localhost');
 define('DB_PORT', 33060);
 define('DB_DATABASE', 'blog');
 define('DB_CHARSET', 'utf8');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
+
+date_default_timezone_set('Asia/Taipei');
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 require __DIR__.'/bootstrap.php';
-require 'vendor/autoload.php';
+
 
 use Carbon\Carbon;
+Carbon::setlocale('zh-TW');
 
 // connect to dabase
 try {
@@ -13,7 +14,7 @@ try {
     echo  "Error: ".$e->getMessage()."＜br/＞";
     die();
 }
-
+//echo Carbon::now('Asia/Taipei');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -84,7 +85,7 @@ try {
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">文章清單
-                    <small><?php echo Carbon::now('Asia/Taipei'); ?></small>
+                    <small><?=Carbon::now()->toDateString();  ?></small>
                 </h1>
             </div>
         </div>
@@ -104,7 +105,7 @@ try {
                 <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
                 <p class="text-right">
                     <span class="glyphicon glyphicon-time"></span>
-                    發表於 <?=$row->created_at?>
+                    發表於 <?=Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->diffForHumans()?>
                 </p>
             </div>
             <?php endwhile; ?>
